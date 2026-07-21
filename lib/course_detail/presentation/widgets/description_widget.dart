5fd2ee1c2109aea0368d8081e846193bbe884dab
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2026-07-21 18:03:11 +0300

    Create course description widget

---
+++ lib/course_detail/presentation/widgets/description_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class DescriptionWidget extends StatelessWidget {
+  final List<String> desc;
+  const DescriptionWidget({super.key, required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'What you will learn',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+
+          ...desc.map(
+            (item) => Padding(
+              padding: const EdgeInsets.only(bottom: 18),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(Icons.check, color: Colors.white, size: 22),
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      item,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        height: 1.6,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
