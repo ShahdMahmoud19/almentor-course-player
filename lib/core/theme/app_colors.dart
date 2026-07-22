de4cec6c32be72b7c5523fe72d4f77b7f81efb58
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2026-07-22 14:06:47 +0300

    Centralize app colors in AppColors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.black;
+  static const Color card =  Color.fromARGB(255, 29, 28, 28);
+  static  Color grey800 = Colors.grey.shade800;
+  static  Color grey400 = Colors.grey.shade400;
+  static  Color grey300 = Colors.grey.shade300;
+  static const Color accent = Colors.white;
+  static const Color secondary = Colors.red;
+}
