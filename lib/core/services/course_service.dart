966900d80a1d8e9ad9ed9027e4868971b3f2e59b
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2026-07-22 14:05:10 +0300

    update data

--- lib/core/services/course_service.dart
+++ lib/core/services/course_service.dart
@@ -8,14 +8,15 @@ class CourseService {
         title: 'Intro to UI/UX Design',
         thumbnailUrl:
             'https://teknovation.io/wp-content/uploads/2023/03/UI-UX-design-Blog.jpg',
-        durationSeconds: 30,
-description: [
-  'Understand the difference between UI and UX design.',
-  'Learn the fundamentals of user-centered design.',
-  'Create simple wireframes and user flows.',
-  'Apply visual design principles such as color, typography, and spacing.',
-  'Design intuitive interfaces that improve user experience.',
-],        videoUrl:
+        durationSeconds: 11,
+        description: [
+          'Understand the difference between UI and UX design.',
+          'Learn the fundamentals of user-centered design.',
+          'Create simple wireframes and user flows.',
+          'Apply visual design principles such as color, typography, and spacing.',
+          'Design intuitive interfaces that improve user experience.',
+        ],
+        videoUrl:
             'https://media.istockphoto.com/id/1345397067/video/black-male-creating-cellphone-app-on-computer.mp4?s=mp4-640x640-is&k=20&c=r9TA2eLze5yT85SwYaB656c5hAZ-2MALfYe4NfDpvUc=',
       ),
       CourseModel(
@@ -23,14 +24,15 @@ description: [
         title: 'Digital Marketing Basics',
         thumbnailUrl:
             'https://pixel-systems.com/assets/images/about/digital-marketing.webp',
-        durationSeconds: 30,
-description: [
-  'Understand the fundamentals of digital marketing.',
-  'Learn the basics of SEO, social media, and email marketing.',
-  'Create effective marketing campaigns for different audiences.',
-  'Measure campaign performance using key marketing metrics.',
-  'Develop a simple digital marketing strategy from scratch.',
-],        videoUrl:
+        durationSeconds: 10,
+        description: [
+          'Understand the fundamentals of digital marketing.',
+          'Learn the basics of SEO, social media, and email marketing.',
+          'Create effective marketing campaigns for different audiences.',
+          'Measure campaign performance using key marketing metrics.',
+          'Develop a simple digital marketing strategy from scratch.',
+        ],
+        videoUrl:
             'https://media.istockphoto.com/id/1333453168/video/adult-woman-leading-a-strategic-business-plan-meeting-pointing-at-the-white-board-while.mp4?b=1&s=192_srp&k=20&c=nudoNZZhbK9DfuB-dTBR3wTU8eiRV3Em1VmqVQmpIdo=',
       ),
       CourseModel(
@@ -38,7 +40,7 @@ description: [
         title: 'Public Speaking Confidence',
         thumbnailUrl:
             'https://th.bing.com/th/id/R.f1978816c861b98ea0cb7152a3afbee3?rik=XR1TotSk9siGHw&pid=ImgRaw&r=0',
-        durationSeconds: 30,
+        durationSeconds: 22,
         description: [
           'Overcome stage fear and speak with confidence.',
           'Improve voice projection, tone, and body language.',
@@ -54,7 +56,7 @@ description: [
         title: 'Excel for Beginners',
         thumbnailUrl:
             'https://media.geeksforgeeks.org/wp-content/uploads/20240701164511/Microsoft-Excel-Courses.webp',
-        durationSeconds: 30,
+        durationSeconds: 17,
         description: [
           'Navigate the Excel interface and understand workbook basics.',
           'Enter, format, and organize data efficiently.',
