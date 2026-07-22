71c98ac39d4a7379c1caef6c49ceb9d70d8a7fb9
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2026-07-22 15:15:28 +0300

    update colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Colors.black;
-  static const Color card =  Color.fromARGB(255, 29, 28, 28);
-  static  Color grey800 = Colors.grey.shade800;
-  static  Color grey400 = Colors.grey.shade400;
-  static  Color grey300 = Colors.grey.shade300;
+  static const Color card = Color.fromARGB(255, 29, 28, 28);
+  static Color grey800 = Colors.grey.shade800;
+  static Color grey400 = Colors.grey.shade400;
+  static Color grey300 = Colors.grey.shade300;
   static const Color accent = Colors.white;
   static const Color secondary = Colors.red;
+
+  static const Color grey = Colors.grey;
 }
